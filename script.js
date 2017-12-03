ee6c4187f9f1634830179358b2441ec4bbdee211
Patrick McCarty <patrickthesailorman@gmail.com>
2017-12-03 16:58:51 +0000

    Add styling to buttons

--- script.js
+++ script.js
@@ -7,12 +7,15 @@
     
     $.getJSON("https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&callback=", function(data) {
   //$(".message").append(data[0].content + "<p>— " + data[0].title + "</p>")
-
+    var message = document.getElementById("quote");
+    var getQuote = document.getElementById("getQuote");
+    var tweet = document.getElementById("twitter");
         $(".message").html(data[0].content + " - " + data[0].title);
+        message.appendChild(getQuote);
         
         $('#twitter').attr('href', 'https://twitter.com/intent/tweet?text=' + data[0].content.slice(3, -5) + ' — ' + data[0].title);
 // }, 'jsonp');
-        
+        message.appendChild(tweet);
     });
     });
 });
